8847e7fdd04f67b20a0de08e2256a45cb19497db
Rashad-Muntar <muntartoure@gmail.com>
2021-09-24 11:43:20 +0000

    Update Origins link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://modest-aryabhata-cbcc3f.netlify.app/'
+    origins 'https://modest-aryabhata-cbcc3f.netlify.app'
 
     resource '*',
       headers: :any,
