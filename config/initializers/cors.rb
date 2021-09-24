107799f5c1cb945446d145f5084d676d578af799
Rashad-Muntar <muntartoure@gmail.com>
2021-09-24 12:43:02 +0000

    origin updated to eroku version

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://modest-aryabhata-cbcc3f.netlify.app'
+    origins 'https://quiet-citadel-65357.herokuapp.com'
 
     resource '*',
       headers: :any,
