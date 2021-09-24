fe59564d29148c48e8fdf0073bc991eba275153e
Rashad-Muntar <muntartoure@gmail.com>
2021-09-24 19:25:46 +0000

    resolve stylint

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -1,25 +1,21 @@
 class Api::ActivitiesController < ApplicationController
+  def index
+    @activities = Activity.all
+    render json: { activities: @activities, message: 'All activities fectched' }
+  end
 
-    def index
-        @activities = Activity.all
-        render json: {activities: @activities, message: 'All activities fectched'}
+  def create
+    @activity = Activity.create(activity_params)
+    if @activity
+      render json: { statues: 'created', activity: @activity, message: 'Activity was successfully created' }
+    else
+      render json: { message: 'Ooops activity was not able to create' }
     end
+  end
 
-    def create
-        @activity = Activity.create(activity_params)
-        if @activity
-            render json: {statues: 'created', activity: @activity, message: 'Activity was successfully created'}
-        else
-            render json: {message: 'Ooops activity was not able to create'}
-        end
-    end
+  private
 
-    def update
-        activity = Activity.update
-    end
-
-    private
-    def activity_params
-        params.require(:activity).permit(:title, :category_id, :user_id)
-    end
+  def activity_params
+    params.require(:activity).permit(:title, :category_id, :user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :categories, dependent: :destroy
-    has_many :activities, dependent: :destroy
-    validates :username, presence: true
+  has_many :categories, dependent: :destroy
+  has_many :activities, dependent: :destroy
+  validates :username, presence: true
 end
